4aead121d96ac11f3872c8b196459e5cc4dded39
Jorge Pedro <jorge.pedro.neto8@gmail.com>
2019-03-01 15:37:45 -0300

    Criando Rota para aplicação do cupom

--- src/main/java/com/app/movile/controller/CouponController.java
+++ src/main/java/com/app/movile/controller/CouponController.java
@@ -1,12 +1,16 @@
 package com.app.movile.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.app.movile.dto.CouponDto;
 import com.app.movile.service.CouponService;
@@ -19,10 +23,18 @@ public class CouponController {
 	private CouponService service;
 	
 	@PostMapping("/create")
-	public RequestEntity<Void> create(@RequestBody CouponDto coupon) {
+	public ResponseEntity<Void> create(@RequestBody CouponDto coupon) {
 		
 		service.create(coupon);
+		
+		return ResponseEntity.created(null).build();
+	}
 	
-		return null;
+	@PostMapping("/apply/{couponCode}/{idCart}")
+	public ResponseEntity<Void>apply(@PathVariable String couponCode, @PathVariable Long idCart) {
+		
+		service.apply(couponCode, idCart);
+		
+		return ResponseEntity.created(null).build();
 	}
 }
