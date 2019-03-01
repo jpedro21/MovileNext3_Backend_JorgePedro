ff5324560aa76365a0cca6d5a012ece9198134f2
Jorge Pedro <jorge.pedro.neto8@gmail.com>
2019-03-01 15:38:52 -0300

    Incluindo invariante

--- src/main/java/com/app/movile/business/CouponBusiness.java
+++ src/main/java/com/app/movile/business/CouponBusiness.java
@@ -1,25 +1,25 @@
 package com.app.movile.business;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.List;
 
-
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.app.movile.coupon.CartBehaviorRestriction;
-import com.app.movile.coupon.Coupon;
 import com.app.movile.coupon.CouponImpl;
 import com.app.movile.coupon.FixedPriceBehaviorAction;
 import com.app.movile.dto.ActionDto;
-import com.app.movile.dto.CartDto;
 import com.app.movile.dto.CouponDto;
-import com.app.movile.dto.FixedPriceActionDto;
 import com.app.movile.dto.RestrictionDto;
 import com.app.movile.entity.Cart;
+import com.app.movile.entity.ItemCart;
+import com.app.movile.entity.Product;
+import com.app.movile.entity.Shipping;
 import com.app.movile.repository.CartBehaviorRestrictionRepository;
+import com.app.movile.repository.CartRepository;
 import com.app.movile.repository.CouponRepository;
 import com.app.movile.repository.FixedPriceBehaviorActionRepository;
 
@@ -35,14 +35,17 @@ public class CouponBusiness {
 	@Autowired
 	private FixedPriceBehaviorActionRepository fixedPriceActRepo;
 	
-	/**
+	@Autowired
+	private CartRepository cartRepo; 
+	
+	/*
 	 * 
 	 * Caso acontece alguma exceção do cadastro do cupom, restrição 
 	 * ou ação toda a transação deverá ser revertida 
 	 */
 	
 	//TODO: Possivelmente este metodo ficará gigante, tentar melhorar; 
-	@Transactional(propagation=Propagation.MANDATORY)
+	@Transactional
 	public void create(CouponDto couponDto) {
 		
 		/**
@@ -50,7 +53,7 @@ public class CouponBusiness {
 		 * Salva cupom. O cupom deve ser salvo antes da restrição
 		 * e ação.
 		 */
-		Coupon coupon = new CouponImpl(couponDto.getCode(), 
+		CouponImpl coupon = new CouponImpl(couponDto.getCode(), 
 									   couponDto.getIniDate(), 
 									   couponDto.getEndDate(),
 									   couponDto.getText());
@@ -64,8 +67,8 @@ public class CouponBusiness {
 		RestrictionDto restrictions = couponDto.getRestrictions();
 		if(restrictions != null) {
 			if(restrictions.getCartRestriction() != null) {
-				CartBehaviorRestriction cartRestr = new CartBehaviorRestriction(coupon.getCode(), 
-																				restrictions.getCartRestriction().getCartPrice());
+				CartBehaviorRestriction cartRestr = new CartBehaviorRestriction(coupon, 
+																				restrictions.getCartRestriction().getMinimumCartPrice());
 				cartRestrRepo.save(cartRestr);
 			}
 		}
@@ -77,21 +80,25 @@ public class CouponBusiness {
 		
 		ActionDto action = couponDto.getAction();
 		if(action.getFixedPriceAction() != null) {
-			FixedPriceBehaviorAction fixedPriceAct = new FixedPriceBehaviorAction(couponDto.getCode(), 
+			FixedPriceBehaviorAction fixedPriceAct = new FixedPriceBehaviorAction(coupon, 
 																				  action.getFixedPriceAction().getDiscount());
 			fixedPriceActRepo.save(fixedPriceAct);
 		}
 	}
 	
-	public CartDto applyCoupon(String code, CartDto cartDto) {
+	public void applyCoupon(String code, Long idCart) {
 		
-		Coupon coupon = couponRepo.findByCode(code);
+		CouponImpl coupon = couponRepo.findByCode(code);
+		
+		if(coupon == null) {
+			throw new IllegalArgumentException("Cupom não encontrado.");
+		}
+		
 		coupon.setRestrictions(Arrays.asList(cartRestrRepo.findByCouponCode(code)));
 		coupon.setAction(fixedPriceActRepo.findByCouponCode(code));
-		
-		ModelMapper mapper = new ModelMapper();
-		Cart cart = coupon.processCoupon(mapper.map(cartDto, Cart.class));
-		return mapper.map(cart, CartDto.class);
+
+		Cart cart = cartRepo.findOneById(idCart);
+		coupon.processCoupon(cart, cartRepo);
 	}
 	
 }
